6de326521b80c36d6941267d0057cb33744f9b98
vijay shankar <botu@vijays-MacBook-Air.local>
2026-01-23 07:54:44 +0530

    Added Public Subnet Route Tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -73,3 +73,25 @@ resource "aws_route_table_association" "private" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.private.id
 }
+
+
+#Public route table 
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.iaac_vpc.id
+    tags = {
+        Name = "${var.name}-public-rt"
+    }
+}
+
+resource "aws_route" "public-igw" {
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+    for_each = aws_subnet.public
+    subnet_id = each.value.id
+    route_table_id = aws_route_table.public
+}
